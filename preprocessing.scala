bf9a4fa53f7825473cddf15bb331d92f486868a1
jeremite <35880425+jeremite@users.noreply.github.com>
2020-09-19 15:13:45 -0700

    Add files via upload

---
+++ preprocessing.scala
@@ -0,0 +1,60 @@
+// Databricks notebook source
+import org.apache.spark.ml.feature.Tokenizer
+import org.apache.spark.ml.feature.RegexTokenizer
+import org.apache.spark.ml.feature.StopWordsRemover
+import org.apache.spark.ml.feature.NGram
+import org.apache.spark.ml.feature.HashingTF
+import org.apache.spark.ml.feature.IDF
+
+// COMMAND ----------
+
+// File location and type
+val file_location = "/FileStore/tables/train.csv"
+val file_type = "csv"
+
+// CSV options
+val infer_schema = "false"
+val first_row_is_header = "true"
+val delimiter = ","
+
+// The applied options are for CSV files. For other file types, these will be ignored.
+val df = spark.read.format(file_type)
+.option("inferSchema", infer_schema)
+.option("header", first_row_is_header)
+.option("sep", delimiter)
+.load(file_location)
+
+// COMMAND ----------
+
+val df_used =df.filter($"question_text".isNotNull)
+
+// COMMAND ----------
+
+val tokenizer = new Tokenizer().setInputCol("question_text").setOutputCol("words")
+val wordsDF = tokenizer.transform(df_used)
+
+// COMMAND ----------
+
+val remover = new StopWordsRemover().setInputCol("words").setOutputCol("filteredWords")
+val noStopWordsDF = remover.transform(wordsDF)
+
+// COMMAND ----------
+
+val ngram = new NGram().setN(2).setInputCol("filteredWords").setOutputCol("ngrams")
+val nGramDF = ngram.transform(noStopWordsDF)
+
+// COMMAND ----------
+
+val hashingTF = new HashingTF().setInputCol("filteredWords").setOutputCol("rawFeatures").setNumFeatures(100)
+val rawFeaturesDF = hashingTF.transform(noStopWordsDF)
+val idf = new IDF().setInputCol("rawFeatures").setOutputCol("features")
+val idfModel = idf.fit(rawFeaturesDF)
+val featuresDF = idfModel.transform(rawFeaturesDF)
+
+// COMMAND ----------
+
+featuresDF.select("question_text","features").show()
+
+// COMMAND ----------
+
+
